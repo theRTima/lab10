942c8ae91cdce9dac2331b7456cb7b89dda0acfb
theRTima <roschintimur@gmail.com>
2026-04-03 23:24:44 +0300

    add 2 simple api's:   mid/1.go

---
+++ mid/1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello there " + name,
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
